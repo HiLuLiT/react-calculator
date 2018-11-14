1ffd10a7efbfc1654ba33058bab5263276d234a4
Hila Ben Baruch <hila.ben-baruch@ironsrc.com>
2018-11-14 23:42:19 +0200

    added auto scale for display

--- app/components/Calculator/Calculator.js
+++ app/components/Calculator/Calculator.js
@@ -3,9 +3,47 @@ import './Calculator.scss';
 import React from 'react';
 import { hot } from 'react-hot-loader';
 
-class AutoShirnkingText extends React.Component {
+class AutoShrink extends React.Component {
+  
+  state ={
+    scale: 1
+  }
+  
+  componentDidUpdate() {
+    const {scale} = this.state;
+
+    const node = this.node;
+    const parentNode = node.parentNode;
+
+    const avilableWidth = parentNode.offsetWidth;
+    const actualWidth = node.offsetWidth;
+    const actualScale = avilableWidth / actualWidth;
+
+    // since setting state in componentDidUpdate, this will prevent looping
+    if (scale === actualScale) {
+      return;
+    }
+
+    if (actualScale < 1) {
+      this.setState({
+        scale: actualScale
+      })
+    } else if ( scale < 1) {
+      this.setState({
+        scale: 1
+      }) 
+    }
+
+  }
+
   render() {
-    return <div {...this.props}/>
+    const {scale} =  this.state;
+    return (
+    <div 
+    {...this.props} 
+    style={{transform: `scale(${scale},${scale})`}}
+    ref={ node=> this.node = node }/>
+    )
   }
 }
 
@@ -108,7 +146,9 @@ class Calculator extends React.Component {
     return (
       
       <div className="calculator">
-        <div className="calculator-display auto-scaling-div">{displayValue}</div>
+       <div className="calculator-display">
+        <AutoShrink className="auto-scaling-div">{displayValue}</AutoShrink>
+       </div>
         <div className="calculator-keypad">
           <div className="input-keys">
 
